c9c9cc6c6a3697d33391bfe88d941f115c57b46f
Gauravski <ghahuja@gmail.com>
2024-10-11 21:10:04 +0530

    Update DataController.java

--- DataController.java
+++ DataController.java
@@ -1,3 +1,10 @@
+package com.yourcompany.ccards.controller;
+
+import com.yourcompany.ccards.service.DataGenerationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/data")
 public class DataController {
@@ -5,14 +12,24 @@ public class DataController {
     @Autowired
     private DataGenerationService dataGenerationService;
 
-    @PostMapping("/generate")
-    public ResponseEntity<String> generateData(@RequestParam int count) {
+    @GetMapping("/generate/{count}")
+    public ResponseEntity<String> generateData(@PathVariable int count) {
         dataGenerationService.generateData(count);
-        return ResponseEntity.ok(dataGenerationService.getDataCounts());
+        return ResponseEntity.ok("Generated data: " + dataGenerationService.getDataCounts());
     }
 
     @GetMapping("/count")
     public ResponseEntity<String> getDataCounts() {
         return ResponseEntity.ok(dataGenerationService.getDataCounts());
+    }
+
+    @GetMapping("/count/cardholders")
+    public ResponseEntity<String> getCardholderCount() {
+        return ResponseEntity.ok("Cardholder count: " + dataGenerationService.getCardholderCount());
+    }
+
+    @GetMapping("/count/cardcreations")
+    public ResponseEntity<String> getCardCreationCount() {
+        return ResponseEntity.ok("Card Creation count: " + dataGenerationService.getCardCreationCount());
     }
 }
